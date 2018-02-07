47c21945dc0e34628f8951928ceadf4593b25273
Aaron <heyitsronex@gmail.com>
2018-02-07 16:18:51 -0500

    working collectionView with DummyRecipe object

---
+++ Broccoholic/RecipeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCollectionViewCell.swift
+//  Broccoholic
+//
+//  Created by Aaron Chong on 2/7/18.
+//  Copyright © 2018 Jonathan Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var recipeImageView: UIImageView!
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    
+}
+
+
--- Broccoholic/ViewController.swift
+++ Broccoholic/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource {
+    
+    var recipeArray = [DummyRecipe]()
+//    var dummyRecipe =
+    
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+        
+        let recipe1 = DummyRecipe(recipeName: "Broccoli Pasta", recipeImage: UIImage(named:"broccoli_pasta")!)
+        let recipe2 = DummyRecipe(recipeName: "Broccoli Salad", recipeImage: UIImage(named:"broccoli_salad")!)
+        let recipe3 = DummyRecipe(recipeName: "Pea & Potato Curry", recipeImage: UIImage(named:"pea_potato_curry")!)
+        let recipe4 = DummyRecipe(recipeName: "Roasted Vegetables", recipeImage: UIImage(named:"roasted_vegetables")!)
+        let recipe5 = DummyRecipe(recipeName: "Spinach Pie", recipeImage: UIImage(named:"spinach_pie")!)
+        
+        self.recipeArray = [recipe1, recipe2, recipe3, recipe4, recipe5]
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return recipeArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recipeCell", for: indexPath) as! RecipeCollectionViewCell
+        
+        let dummyRecipe = self.recipeArray[indexPath.item]
+        
+        cell.recipeImageView.image = dummyRecipe.recipeImage
+        cell.recipeNameLabel.text = dummyRecipe.recipeName
+        
+        return cell
+        
+    }
 }
 
