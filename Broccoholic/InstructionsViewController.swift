efb46e5d12fdfe437c4ca9aee60323f6d2cb2627
Aaron <heyitsronex@gmail.com>
2018-02-11 16:31:31 -0500

    refactor fade animation

--- Broccoholic/InstructionsViewController.swift
+++ Broccoholic/InstructionsViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class InstructionsViewController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var instructionsTextLabel: UILabel!
-    
+    @IBOutlet weak var instructionNumber: UILabel!
     var instructionsArray: [String]!
     var position = 0
     
@@ -27,37 +27,22 @@ class InstructionsViewController: UIViewController, UIGestureRecognizerDelegate 
         print("leftSwiped")
         if position == instructionsArray.index(of: instructionsArray.last!)! {
             
-            UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
-                self.instructionsTextLabel.alpha = 0.0
-            
-            }, completion: {
+            self.instructionsTextLabel.fadeOut(completion: {
                 (finished: Bool) -> Void in
-                
                 self.instructionsTextLabel.text = self.instructionsArray.first!
-                
-                UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
-                    self.instructionsTextLabel.alpha = 1.0
-                }, completion: nil)
+                self.instructionsTextLabel.fadeIn()
             })
-            
             position = 0
+            
         } else {
             
             position += 1
             
-            UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
-                self.instructionsTextLabel.alpha = 0.0
-            }, completion: {
+            self.instructionsTextLabel.fadeOut(completion: {
                 (finished: Bool) -> Void in
-        
                 self.instructionsTextLabel.text = self.instructionsArray[self.position]
-            
-                UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
-                    self.instructionsTextLabel.alpha = 1.0
-                }, completion: nil)
-            
+                self.instructionsTextLabel.fadeIn()
             })
-        
         }
     }
     
@@ -66,37 +51,22 @@ class InstructionsViewController: UIViewController, UIGestureRecognizerDelegate 
         print("rightSwiped")
         if position == instructionsArray.index(of: instructionsArray.first!)! {
             
-            UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
-                self.instructionsTextLabel.alpha = 0.0
-            }, completion: {
+            self.instructionsTextLabel.fadeOut(completion: {
                 (finished: Bool) -> Void in
-                
-            self.instructionsTextLabel.text = self.instructionsArray.last!
-            
-                UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
-                    self.instructionsTextLabel.alpha = 1.0
-                }, completion: nil)
+                 self.instructionsTextLabel.text = self.instructionsArray.last!
+                self.instructionsTextLabel.fadeIn()
             })
-            
             position = instructionsArray.count-1
+            
         } else {
             
             position -= 1
             
-            UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
-                self.instructionsTextLabel.alpha = 0.0
-            }, completion: {
+            self.instructionsTextLabel.fadeOut(completion: {
                 (finished: Bool) -> Void in
-                
                 self.instructionsTextLabel.text = self.instructionsArray[self.position]
-                
-                UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
-                    self.instructionsTextLabel.alpha = 1.0
-                }, completion: nil)
+                self.instructionsTextLabel.fadeIn()
             })
         }
     }
-    
-    
-    
 }
---
+++ Broccoholic/UIViewExtensions.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewExtensions.swift
+//  Broccoholic
+//
+//  Created by Aaron Chong on 2/11/18.
+//  Copyright © 2018 Jonathan Oliveira. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func fadeIn(_ duration: TimeInterval = 0.3, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: completion)  }
+    
+    func fadeOut(_ duration: TimeInterval = 0.3, delay: TimeInterval = 0.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.alpha = 0.0
+        }, completion: completion)
+    }
+}
