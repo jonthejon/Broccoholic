8309192c05d39dc8b651a6b41b10721b665c33eb
Jonathan Oliveira <jon.brasileiro@gmail.com>
2018-02-11 17:20:53 -0500

    creates the mapview and have it display on screen

---
+++ Broccoholic/MapViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MapViewController.swift
+//  Broccoholic
+//
+//  Created by Jonathan Oliveira on 11/02/18.
+//  Copyright © 2018 Jonathan Oliveira. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+	@IBOutlet weak var map: MKMapView!
+	let locationManager = CLLocationManager()
+	
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+		self.locationManager.requestWhenInUseAuthorization()
+		self.map.showsUserLocation = true
+		self.locationManager.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//		CLLocation *currentLocation = locations[0];
+		let currentLocation = locations[0]
+//		[self.mapView setRegion:MKCoordinateRegionMake(currentLocation.coordinate, MKCoordinateSpanMake(0.06, 0.06))];
+		self.map.setRegion(MKCoordinateRegionMake(currentLocation.coordinate, MKCoordinateSpanMake(0.06, 0.06)), animated: true)
+//		[NetworkManager yelpSearchWithLocation:currentLocation.coordinate completionHandler:^(NSArray<Cafe *> * cafes) {
+//			self.cafes = cafes;
+//			[self.mapView addAnnotations:self.cafes];
+//			[self.mapView showAnnotations:self.cafes animated:YES];
+//			}];
+	}
+
+	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+		print("Something weird happened here!")
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+		if status == .authorizedWhenInUse || status == .authorizedAlways {
+			self.locationManager.requestLocation()
+		}
+	}
+}
